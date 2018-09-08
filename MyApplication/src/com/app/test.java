410ff1462a326b91655ab3db4dd6ec35ac2e7e3f
user1 <user1@user>
2018-09-08 12:14:50 +0530

    SecondEdit

--- MyApplication/src/com/app/test.java
+++ MyApplication/src/com/app/test.java
@@ -3,9 +3,12 @@ package com.app;
 public class test {
 
 	public static void main(String[] args) {
+		int i=10;
 		// TODO Auto-generated method stub
 System.out.println("hello");
 System.out.println("welcome");
+System.out.println(i);
+
 	}
 
 }
