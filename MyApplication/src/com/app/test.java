c405736d09cdc05adce436a1b32b33c2765f3fb4
user1 <user1@user>
2018-09-08 12:21:49 +0530

    edit2

--- MyApplication/src/com/app/test.java
+++ MyApplication/src/com/app/test.java
@@ -5,8 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		int i=10;
 		// TODO Auto-generated method stub
-System.out.println("hello");
-System.out.println("welcome");
+
 System.out.println("value is"+i);
 
 	}
