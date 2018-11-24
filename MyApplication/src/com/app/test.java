b08a6eb0af8bd30c454fa4c92903d415d993803a
user1 <user1@user>
2018-11-24 17:40:23 +0530

    add sysout

--- MyApplication/src/com/app/test.java
+++ MyApplication/src/com/app/test.java
@@ -3,18 +3,8 @@ package com.app;
 public class test {
 
 	public static void main(String[] args) {
-		int i=10;
 		// TODO Auto-generated method stub
-
-System.out.println("value is"+i);
-
-
-Employee e=new Employee();
-e.setEmpid(10);
-e.setEmpname("cn");
-e.setEmpsal(6.6);
-System.out.println(e);
-System.out.println(e);
+System.out.println("hello");
 	}
-
+System.out.println("hi");
 }
