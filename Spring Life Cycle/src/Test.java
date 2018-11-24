82e64562494493670169087f1610a6c6415d0e81
user1 <user1@user>
2018-11-24 17:28:49 +0530

    add sysout

--- Spring Life Cycle/src/Test.java
+++ Spring Life Cycle/src/Test.java
@@ -6,6 +6,7 @@ import org.springframework.util.SystemPropertyUtils;
 public class Test {
 
 	public static void main(String[] args) {
+		System.out.println("hi");
 		// TODO Auto-generated method stub
 AbstractApplicationContext a=new ClassPathXmlApplicationContext("spring.xml");
 Employee e=(Employee)a.getBean("e");
@@ -13,4 +14,5 @@ System.out.println(e);
 a.registerShutdownHook();
 	}
 
+	
 }
